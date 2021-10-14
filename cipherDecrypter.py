481562768b2d703e5cffd89206664845c673cbbb
Joshua Greaves <58918808+jtgreaves@users.noreply.github.com>
2021-10-14 16:08:04 +0100

    Update cipherDecrypter.py

--- cipherDecrypter.py
+++ cipherDecrypter.py
@@ -1,28 +1,65 @@
-userSentence = input("What would you like decrypt? ").lower()
-print(userSentence)
 
-userSentence = userSentence.split(" ")
-finalSentence = []
+#userSentence = input("What would you like decrypt? ").lower()
+#print(userSentence)
+#
+#userSentence = userSentence.split(" ")
+#finalSentence = []
+#
+#i = 0 
+#while i < len(userSentence): # Loop through the words 
+#	p = 0
+#	finalSentence.append([])
+#	
+#	while p < len(userSentence[i]): # Loop through the letters
+#		print("#DEBUG P", finalSentence)
+#		x = 0
+#		print(p)
+#		finalSentence[i].append([])
+#		
+#		while x < 26: # Calculate all cipher posibilities 
+#			print (userSentence[i][p], x)
+#			finalSentence[i][p] += chr(ord(userSentence[i][p]) + x)#THIS NEEDS TO BE SEPARATED SO YOU DO NOT OVERFLOW
+#			x += 1
+#	
+#		p += 1
+#	
+#	print(userSentence[i])
+#	i += 1 
+#	
+#print(finalSentence)
+#
 
-i = 0 
-while i < len(userSentence): # Loop through the words 
-	p = 0
-	finalSentence.append([])
+
+def cipherSentence(sentence):
+	sentenceVariations = []
+	words = sentence.split(" ")
 	
-	while p < len(userSentence[i]): # Loop through the letters
-		print("#DEBUG P", finalSentence)
-		x = 0
-		print(p)
-		finalSentence[i].append([])
+	print(words)
+	counter = 0 
+	for word in words:
+		sentenceVariations.append([])
 		
-		while x < 26: # Calculate all cipher posibilities 
-			print (userSentence[i][p], x)
-			finalSentence[i][p] += chr(ord(userSentence[i][p]) + x)#THIS NEEDS TO BE SEPARATED SO YOU DO NOT OVERFLOW
-			x += 1
+		i = 0 
+		while i < 26: 
+			
+		sentenceVariations[counter].append(cipherWord(word))
+		counter =+ 1
+		
+	return sentenceVariations
 	
-		p += 1
 	
-	print(userSentence[i])
-	i += 1 
+def cipherWord(word):
 	
-print(finalSentence)
+	for x in word: 
+		print(ord(x))
+	 
+	return word
+	
+	
+def dictionaryCheck(word):
+	print(dictionary)
+	
+
+sentence = input("What sentence would you like to decrypt? ").lower()
+print(cipherSentence(sentence))
+
