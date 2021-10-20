ff2c3fd524a17ee770888fff61d755564620fbb5
Joshua Greaves <58918808+jtgreaves@users.noreply.github.com>
2021-10-20 18:52:59 +0100

    Cleanup

--- Homework/week 5.py
+++ Homework/week 5.py
@@ -1,24 +1,30 @@
 # Week 5 HW - Joshua Greaves
 
 numbers = [] 
-while len(numbers) < 102: #102 as people need to be able to select number 0 & 101
+while len(numbers) < 101:
     numbers.append("")
 
+used = 0 
 total = len(numbers)
 turn = "A"
 while total > -1:
     turnOver = False
     while turnOver == False:
-        number = int()
-        while number > -1 and number < 102: 
+        number = int(input("What number? "))
+        while number < 0 or number > 100: 
+            print("That number is invalid!")
             number = int(input("What number? "))
         if numbers[number] == "": 
             numbers[number] = turn 
             total -= 1 
             turnOver = True
+            print(numbers)
         else: 
             print("Sorry that is already taken! Please pick another one")
 
     if turn == "A": turn = "B"
     elif turn == "B": turn = "C"
     else: turn = "A"
+    used += 1 
+
+    print("You have", str(len(numbers) - used), "left!") 
