77e238d828e9e8421edd47ca254096fb9f926687
Joshua Greaves <58918808+jtgreaves@users.noreply.github.com>
2021-11-05 09:58:51 +0000

    Add files via upload

--- C4.py
+++ C4.py
@@ -1,22 +1,42 @@
-grid = [["", "", "", "", "", ""],
-["", "", "", "", "", ""],
-["", "", "", "", "", ""],
-["", "", "", "", "", ""],
-["", "", "", "", "", ""],
-["", "", "", "", "", ""],
-["", "", "", "", "", ""],
-["", "", "", "", "", ""],
-["", "", "", "", "", ""]]
+import pygame 
+pygame.init()
 
-player = "R" 
-col = 1 
+width = 700
+height = 600
 
-i = len(grid)-1 
-while i >= 0: 
-	if grid[i][col] == "": 
-		grid[i][col] = player
-		i = -1
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+endProgram = False
+
+def updateScreen(grid):
+	row = 0 
+	while row < len(grid): 
+		column = 0 
+		while column < len(grid[row]):
+			x = (55 * column) + 25
+			y = (55 * row) + 25
+			if grid[row][column] == "R": colour = (255, 0, 0)
+			elif grid[row][column] == "Y": colour = (200, 200, 50)
+			elif grid[row][column] == "B": colour = (255, 255, 255)
+			pygame.draw.circle(screen, colour, (x,y),25)
+			column += 1
+		row += 1 
+
 	
-	i -= 1
-print(grid)
+	
+grid = [["R", "", "", "", "", ""],["", "", "", "", "", ""],["", "", "", "", "", ""],["", "", "", "", "", ""],["", "", "", "", "", ""],["", "", "", "", "", ""],["", "", "", "", "", ""],["", "", "", "", "", ""]]
+while endProgram == False:
+	for e in pygame.event.get(): 
+		if e.type == pygame.QUIT:
+			endProgram = True
+
+		
+	screen.fill((50, 50, 50))
+	
+	updateScreen(grid) 
+
+	
+	pygame.display.update()
+	clock.tick(30)
+
 
